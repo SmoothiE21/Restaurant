573a51485197ff55bb73981b83802307e838de7a
SmoothiE21 <32088283+SmoothiE21@users.noreply.github.com>
2020-11-18 11:49:44 +0200

    DataBinding, worked after crashed
    Data binding, logok a verbosenál
    Egyszer lefutott, működött a fragment váltás, de ezután kicrashel.

--- app/src/main/java/com/example/restaurant_app/MainActivity.kt
+++ app/src/main/java/com/example/restaurant_app/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.restaurant_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.restaurant_app.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //adat elérés
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        //mozogni a fragmentek kozott
+        val navController = findNavController(R.id.myNavHostFragment)
+        // a menühöz, e miatt kell a data binding
+        val bottomNav = binding.bottomNavigationView
+        //bottom nav inicializálás
+        bottomNav?.setupWithNavController(navController)
     }
 }
--- app/src/main/java/com/example/restaurant_app/ProfileFragment.kt
+++ app/src/main/java/com/example/restaurant_app/ProfileFragment.kt
@@ -1,25 +1,30 @@
 package com.example.restaurant_app
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-
-
+import androidx.databinding.DataBindingUtil
+import com.example.restaurant_app.databinding.FragmentProfileBinding
 
 
 class ProfileFragment : Fragment() {
 
-
+private lateinit var binding: FragmentProfileBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false)
+        //bindig értékadása
+        val name=  binding.editTextTextPersonName.text.toString()
+        Log.d("Helo",  name)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_profile, container, false)
+
+        return binding.root
     }
 
 
